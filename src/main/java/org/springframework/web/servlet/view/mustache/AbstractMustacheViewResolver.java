e581ba661244e46caf74706d1ff9b23b460b5799
Kpacha <kpacha666@gmail>
2012-12-28 13:16:43 +0100

    Enabling exclusion by pattern

--- src/main/java/org/springframework/web/servlet/view/mustache/AbstractMustacheViewResolver.java
+++ src/main/java/org/springframework/web/servlet/view/mustache/AbstractMustacheViewResolver.java
@@ -1,7 +1,10 @@
 package org.springframework.web.servlet.view.mustache;
 
+import java.util.Locale;
+
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.util.PatternMatchUtils;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.AbstractTemplateViewResolver;
 import org.springframework.web.servlet.view.AbstractUrlBasedView;
@@ -12,6 +15,8 @@ public abstract class AbstractMustacheViewResolver extends
 	AbstractTemplateViewResolver implements ViewResolver, InitializingBean {
 
     private MustacheTemplateLoader templateLoader;
+
+    private String[] excludedViewNames = null;
 
     @Override
     protected AbstractUrlBasedView buildView(String viewName) throws Exception {
@@ -36,6 +41,45 @@ public abstract class AbstractMustacheViewResolver extends
     @Required
     public void setTemplateLoader(MustacheTemplateLoader templateLoader) {
 	this.templateLoader = templateLoader;
+    }
+
+    /**
+     * Indicates whether or not this
+     * {@link org.springframework.web.servlet.ViewResolver} can handle the
+     * supplied view name. If not, {@link #createView(String, java.util.Locale)}
+     * will return <code>null</code>.
+     * 
+     * This implementation also checks against the configured
+     * {@link #setExcludedExtensions extensions to exclude from the view names}.
+     * 
+     * @param viewName
+     *            the name of the view to retrieve
+     * @param locale
+     *            the Locale to retrieve the view for
+     * @return whether this resolver applies to the specified view
+     * @see org.springframework.util.PatternMatchUtils#simpleMatch(String,
+     *      String)
+     */
+    protected boolean canHandle(String viewName, Locale locale) {
+	String[] excludedViewNames = getExcludedViewNames();
+	boolean isExcluded = (excludedViewNames != null && PatternMatchUtils
+		.simpleMatch(excludedViewNames, viewName));
+	return super.canHandle(viewName, locale) && !isExcluded;
+    }
+
+    /**
+     * @return the excludedViewNames
+     */
+    protected String[] getExcludedViewNames() {
+	return excludedViewNames;
+    }
+
+    /**
+     * @param excludedViewNames
+     *            the excludedExtensions to set
+     */
+    public void setExcludedViewNames(String[] excludedViewNames) {
+	this.excludedViewNames = excludedViewNames;
     }
 
 }
